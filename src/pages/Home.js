702d82de85c81e4f3f98d243800c417b0312b58d
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-19 09:50:26 +0300

    style: navigate programmatically

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 function Home() {
-  return (
-    <div>
-        <h1>Home page</h1>
-        <p> Go to <Link to='products'>Products</Link></p>
-    </div>
-  )
+    const navigate = useNavigate()
+
+    const navigateHandler = () => {
+        navigate('/products')
+    }
+    return (
+        <div>
+            <h1>Home page</h1>
+            <p> Go to <Link to='products'>Products</Link></p>
+            <p>
+                <button onClick={navigateHandler}>Navigate</button>
+            </p>
+        </div>
+    )
 }
 
 export default Home
