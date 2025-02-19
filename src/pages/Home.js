1f12421da42f343877fa015ab06be35892508a44
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-19 09:09:05 +0300

    style: add link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
     <div>
         <h1>Home page</h1>
+        <p> Go to <Link to='products'>Products</Link></p>
     </div>
   )
 }
