b976a0fd0e69603af0b9d75d5d67e1ae1efe17cb
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-19 10:05:56 +0300

    style: generate dynamic routes

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
+const productList = [
+    { id: 'eb8bd792-eca9-4100-a6d8-52722b376c6c', title: 'Product 1'},
+    { id: '25061f54-ac2e-4453-b9db-e0238faa5c90', title: 'Product 2'},
+    { id: '72bac53b-8add-4d65-b35b-485e7efe91a1', title: 'Product 3'},
+    { id: '73e308d7-e87a-406d-8b11-2ce220f407cc', title: 'Product 4'},
+]
 function Products() {
   return (
     <div>
         <h1>Products page</h1>
         <ul>
-            <li>Product 1</li>
-            <li>Product 2</li>
-            <li>Product 3</li>
-            <li>Product 4</li>
+            {productList.map((product) => (
+                <li>
+                    <Link to={`/products/${product.id}`}>
+                        {product.title}
+                    </Link>
+                </li>))
+            } 
         </ul>
     </div>
   )
